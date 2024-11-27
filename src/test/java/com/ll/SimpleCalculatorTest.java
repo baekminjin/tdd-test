9d791e10f312d30b016a2cd851e45757468c9924
baekminjin <bhs3392@naver.com>
2024-11-27 16:27:39 +0900

    red

---
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -0,0 +1,18 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleCalculatorTest {
+	@Test
+	@DisplayName("1+2=3")
+	public void testPlus() {
+		SimpleCalculator simpleCalculator = new SimpleCalculator();
+		int rs = simpleCalculator.plus(1, 2);
+
+		assertEquals(3, rs); //예상값과 나와야하는 값
+	}
+
+}
