22a82f8f67e740f090065b675e91df0883748da3
baekminjin <bhs3392@naver.com>
2024-11-27 16:39:46 +0900

    green

--- src/test/java/com/ll/SimpleCalculatorTest.java
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -8,11 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SimpleCalculatorTest {
 	@Test
 	@DisplayName("1+2=3")
-	public void testPlus() {
+	public void t1() {
 		SimpleCalculator simpleCalculator = new SimpleCalculator();
 		int rs = simpleCalculator.plus(1, 2);
 
 		assertEquals(3, rs); //예상값과 나와야하는 값
+	}
+
+	@Test
+	@DisplayName("2+10=13")
+	public void t2() {
+		SimpleCalculator simpleCalculator = new SimpleCalculator();
+		int rs = simpleCalculator.plus(2, 10);
+
+		assertEquals(12, rs); //예상값과 나와야하는 값
 	}
 
 }
